61356fd679afa54f9f35fda5c6461d1f9e4678f1
zhouy <15955655592>
2023-05-08 18:09:33 +0800

    更新启动容器命令

--- server-init.sh
+++ server-init.sh
@@ -12,4 +12,5 @@ export Ali_Secret=""
 sudo docker login --username=17333255246 registry.cn-shanghai.aliyuncs.com
 
 # 下载镜像、启动容器
-docker run --name yukikyu-homepage  -p 80:80 -p 443:443 -v /root/.acme.sh/yukikyu.com:/etc/nginx/ssl/yukikyu.com registry.cn-hangzhou.aliyuncs.com/yukikyu-namespace/yukikyu-homepage:v1.0.0
+# docker run --name yukikyu-homepage  -p 80:80 -p 443:443 -v /root/.acme.sh/yukikyu.com:/etc/nginx/ssl/yukikyu.com registry.cn-hangzhou.aliyuncs.com/yukikyu-namespace/yukikyu-homepage:v1.0.0
+docker run --name yukikyu-homepage  -p 80:80 -p 443:443 -v /root/.acme.sh/yukikyu.com_ecc:/etc/nginx/ssl/yukikyu.com registry.cn-hangzhou.aliyuncs.com/yukikyu-namespace/yukikyu-homepage:v1.0.0
